b2475362edfddc29a86baebab0aa0a4acf9a8abc
Sophie-Byron <148785265+Sophie-Byron@users.noreply.github.com>
2024-01-17 09:34:16 -0600

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -2,9 +2,18 @@ package program12;
 
 public interface Queue 
 {
+	//This method tests whether a list is empty or not.
 	boolean isEmpty();
+
+	//returns an object, will get the next element in the list.
 	Object dequeue();
+
+	//returns an object, will add an element to the front of the list.
 	Object enqueue(Object e);
+
+	//returns an object, will get an element from the back of the list.
 	Object getBack();
+
+	//returns and object, will get an element from the front of the list.
 	Object getFront();
 }
