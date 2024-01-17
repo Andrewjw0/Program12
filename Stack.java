87d0fa6332ef8e83eff1624eaf7f8ff5e1015d93
Sophie-Byron <148785265+Sophie-Byron@users.noreply.github.com>
2024-01-17 09:30:58 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -2,9 +2,18 @@ package program12;
 
 public interface Stack 
 {
+	//This method tests whether a list is empty or not
 	boolean isEmpty();
-	Object push(Object e);
-	Object pop();
-	Object peek();
-	int search(Object e);
+
+	//returns an object, adds an element to the front of the list.
+	Object push(Object e);	
+
+	//returns an object, gets an element from the front of the list.
+	Object pop();		
+
+	//returns an object, gets an element from the front of the list and adds the element to the front of the list.
+	Object peek();		
+
+	//returns an int,
+	int search(Object e);	
 }
